c47867c0041c4255a3b220b8b1f1217faa1c42b7
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2022-12-28 15:45:00 +0500

    fix: remove uploader runner from sdk instance

--- onlyoffice-integration-sdk/src/main/java/core/OnlyofficeIntegrationSDK.java
+++ onlyoffice-integration-sdk/src/main/java/core/OnlyofficeIntegrationSDK.java
@@ -8,57 +8,10 @@ import core.runner.OnlyofficeEditorRunner;
 import core.uploader.OnlyofficeUploaderRunner;
 import lombok.RequiredArgsConstructor;
 
-/**
- * <pre>ONLYOFFICE Default Pre-processors:</pre>
- * <h3>OnlyofficeEditorDefaultPreProcessor Invocation scheme:</h3>
- * { <br>
- *     <pre>&nbsp;"invocation_key": "onlyoffice.preprocessor.default.editor", </pre>
- *     <pre>&nbsp;"description": "", </pre>
- *     <pre>&nbsp;"type": "preprocessor", </pre>
- *     <pre>&nbsp;"parameters": { </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;"key": { </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"description": "", </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"type": "String" </pre>
- *        <pre>&nbsp;&nbsp;&nbsp;}, </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;"token": { </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"description": "", </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"type": "String" </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;}, </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;"mutator": { </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"description": "", </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"type": "OnlyofficeModelMutator<Config>" </pre>
- *         <pre>&nbsp;&nbsp;} </pre>
- *     <pre>&nbsp;}, </pre>
- *     <pre>&nbsp;"required": ["key", "token"] </pre>
- * } <br>
- * <br>
- * <h3>OnlyofficeCallbackDefaultPreProcessor Invocation scheme:</h3>
- * { <br>
- *     <pre>&nbsp;"invocation_key": "onlyoffice.preprocessor.default.callback", </pre>
- *     <pre>&nbsp;"description": "", </pre>
- *     <pre>&nbsp;"type": "preprocessor", </pre>
- *     <pre>&nbsp;"parameters": { </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;"key": { </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"description": "", </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"type": "String" </pre>
- *        <pre>&nbsp;&nbsp;&nbsp;}, </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;"token": { </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"description": "", </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"type": "String" </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;}, </pre>
- *         <pre>&nbsp;&nbsp;&nbsp;"mutator": { </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"description": "", </pre>
- *             <pre>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"type": "OnlyofficeModelMutator<Callback>" </pre>
- *         <pre>&nbsp;&nbsp;} </pre>
- *     <pre>&nbsp;}, </pre>
- *     <pre>&nbsp;"required": ["key", "token"] </pre>
- * } <br>
- */
 @RequiredArgsConstructor
 public class OnlyofficeIntegrationSDK {
     public final OnlyofficeCallbackRunner callbackRunner;
     public final OnlyofficeEditorRunner editorRunner;
-    public final OnlyofficeUploaderRunner<ConverterRequest> converterRunner;
     public final OnlyofficeCommandClient commandClient;
     public final OnlyofficeConverterClient converterClient;
 }
