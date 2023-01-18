85ec1aa25e4ec9c9ed394c5736e9cd02582dae56
VGKurguzov <vladimir.kurguzov@onlyoffice.com>
2023-01-18 21:02:42 +0300

    add OnlyofficeInjector

---
+++ onlyoffice-integration-sdk/src/main/java/base/injector/OnlyofficeInjector.java
@@ -0,0 +1,10 @@
+package base.injector;
+
+import com.google.inject.AbstractModule;
+
+public class OnlyofficeInjector extends AbstractModule {
+
+    @Override
+    protected void configure() {
+    }
+}
