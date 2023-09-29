f7a98c91f335d9f6f20316f85a2db72f603122cb
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-09-29 16:41:44 +0300

    added: DocumentManager

---
+++ src/main/java/com/onlyoffice/manager/document/DefaultDocumentManager.java
@@ -0,0 +1,276 @@
+/**
+ *
+ * (c) Copyright Ascensio System SIA 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.onlyoffice.manager.document;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onlyoffice.manager.settings.SettingsManager;
+import com.onlyoffice.model.config.document.documenttype.DocumentType;
+import com.onlyoffice.model.format.Format;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DefaultDocumentManager implements DocumentManager {
+    private static final int DEFAULT_MAX_FILE_SIZE = 5242880;
+    private SettingsManager settingsManager;
+    private static List<Format> formats;
+
+    static {
+        init();
+    }
+
+    public List<Format> getFormats() {
+        return formats;
+    }
+
+    public String getDocumentKey(final String fileId, boolean embedded) {
+        return String.valueOf(fileId.hashCode());
+    }
+
+    public String getDocumentName(final String fileId) {
+        return null;
+    }
+
+
+    public String getExtension(final String fileName) {
+        return FilenameUtils.getExtension(fileName).toLowerCase();
+    }
+
+    public String geBaseName(final String fileName) {
+        return FilenameUtils.getBaseName(fileName);
+    }
+
+    public String getMimeType(final String fileName) {
+        Path path = new File(fileName).toPath();
+
+        try {
+            return Files.probeContentType(path);
+        } catch (IOException e) {
+            return "application/octet-stream";
+        }
+    }
+
+    public DocumentType getDocumentType(final String fileName) {
+        String fileExtension = getExtension(fileName);
+
+        for (Format format : this.formats) {
+            if (format.getName().equals(fileExtension)) {
+                return format.getType();
+            }
+        }
+
+        return null;
+    }
+
+    public boolean isEditable(final String fileName) {
+        return hasAction(fileName, "edit");
+    }
+
+    public boolean isViewable(final String fileName) {
+        return hasAction(fileName, "view");
+    }
+
+    public boolean isFillable(final String fileName) {
+        return hasAction(fileName, "fill");
+    }
+
+
+    public boolean hasAction(final String fileName, final String action) {
+        String fileExtension = getExtension(fileName);
+
+        for (Format format : this.formats) {
+            if (format.getName().equals(fileExtension) && format.getActions().contains(action)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public InputStream getNewBlankFile(final String extension, final Locale locale) {
+        String path = "assets/document-templates/" + locale.toLanguageTag() + "/new." + extension;
+
+        if (this.getClass().getClassLoader().getResourceAsStream(path) == null) {
+            path = "assets/document-templates/en-US/new." + extension;
+        }
+
+        return this.getClass()
+                .getClassLoader()
+                .getResourceAsStream(path);
+    }
+
+    public String getDefaultExtension(final DocumentType documentType) {
+        switch (documentType) {
+            case WORD:
+                return "docx";
+            case CELL:
+                return "xlsx";
+            case SLIDE:
+                return "pptx";
+        }
+
+        return null;
+    }
+
+    public String getDefaultConvertExtension(final String fileName) {
+        String extension = getExtension(fileName);
+
+        for (Format format : this.formats) {
+            if (format.getName().equals(extension)) {
+                switch (format.getType()) {
+                    case WORD:
+                        if (format.getName().equals("docxf") && format.getConvert().contains("oform")) {
+                            return "oform";
+                        }
+                        if (format.getConvert().contains("docx")) {
+                            return "docx";
+                        }
+                        break;
+                    case CELL:
+                        if (format.getConvert().contains("xlsx")) {
+                            return "xlsx";
+                        }
+                        break;
+                    case SLIDE:
+                        if (format.getConvert().contains("pptx")) {
+                            return "pptx";
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public List<String> getConvertExtensionList(final String fileName) {
+        String extension = getExtension(fileName);
+
+        for (Format format : formats) {
+            if (format.getName().equals(extension)) {
+                return format.getConvert();
+            }
+        }
+
+        return null;
+    }
+
+    public List<String> getLossyEditableExtensions() {
+        List<String> result = new ArrayList<>();
+
+        for (Format format : formats) {
+            if (format.getActions().contains("lossy-edit")) {
+                result.add(format.getName());
+            }
+        }
+
+        return result;
+    }
+
+    public List<String> getInsertImageExtension() {
+        String insertImage = settingsManager.getSDKSetting("integration-sdk.data.formats.insert-image");
+
+        if (insertImage != null & !insertImage.isEmpty()) {
+            Arrays.asList(insertImage.split("|"));
+        }
+
+        return null;
+    }
+
+    public List<String> getCompareFileExtension() {
+        List<Format> supportedFormats = formats;
+        List<String> result = new ArrayList<>();
+
+        for (Format format : supportedFormats) {
+            if (format.getType().equals(DocumentType.WORD)) {
+                result.add(format.getName());
+            }
+        }
+
+        return result;
+    }
+
+    public List<String> getMailMergeExtension() {
+        List<Format> supportedFormats = formats;
+        List<String> result = new ArrayList<>();
+
+        for (Format format : supportedFormats) {
+            if (format.getType().equals(DocumentType.CELL)) {
+                result.add(format.getName());
+            }
+        }
+
+        return result;
+    }
+
+    public long getMaxFileSize() {
+        long size;
+        try {
+            String filesizeMax = settingsManager.getSDKSetting("integration-sdk.data.filesize.editing.max");
+            size = Long.parseLong(filesizeMax);
+        } catch (Exception ex) {
+            size = 0;
+        }
+
+        return size > 0 ? size : DEFAULT_MAX_FILE_SIZE;
+    }
+
+    public long getMaxConversionFileSize() {
+        long size;
+        try {
+            String filesizeMax = settingsManager.getSDKSetting("integration-sdk.data.filesize.conversion.max");
+            size = Long.parseLong(filesizeMax);
+        } catch (Exception ex) {
+            size = 0;
+        }
+
+        return size > 0 ? size : DEFAULT_MAX_FILE_SIZE;
+    }
+
+    protected static void init() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        InputStream inputStream =  Thread
+                .currentThread()
+                .getContextClassLoader()
+                .getResourceAsStream("assets/document-formats/onlyoffice-docs-formats.json");
+        try {
+            formats = objectMapper.readValue(inputStream, new TypeReference<List<Format>>() { });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/onlyoffice/manager/document/DocumentManager.java
@@ -0,0 +1,50 @@
+/**
+ *
+ * (c) Copyright Ascensio System SIA 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.onlyoffice.manager.document;
+
+import com.onlyoffice.model.config.document.documenttype.DocumentType;
+import com.onlyoffice.model.format.Format;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Locale;
+
+public interface DocumentManager {
+    List<Format> getFormats();
+    String getDocumentKey(String fileId, boolean embedded);
+    String getDocumentName(String fileId);
+    String getExtension(String fileName);
+    String geBaseName(String fileName);
+    String getMimeType(String fileName);
+    DocumentType getDocumentType(String fileName);
+    boolean isEditable(String fileName);
+    boolean isViewable(String fileName);
+    boolean isFillable(String fileName);
+    boolean hasAction(String fileName, String action);
+    InputStream getNewBlankFile(final String extension, final Locale locale);
+    String getDefaultExtension(DocumentType documentType);
+    String getDefaultConvertExtension(final String fileName);
+    List<String> getConvertExtensionList(String fileName);
+    List<String> getLossyEditableExtensions();
+    List<String> getInsertImageExtension();
+    List<String> getCompareFileExtension();
+    List<String> getMailMergeExtension();
+    long getMaxFileSize();
+    long getMaxConversionFileSize();
+}
