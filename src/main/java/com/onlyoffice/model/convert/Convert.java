549ed51c3772fa486cb85a219268c82057c21345
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-10-05 15:04:02 +0300

    added: model Convert

---
+++ src/main/java/com/onlyoffice/model/convert/Convert.java
@@ -0,0 +1,32 @@
+package com.onlyoffice.model.convert;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.onlyoffice.model.convert.thumbnail.Thumbnail;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Convert {
+    private Boolean async;
+    private int codePage;
+    private int delimiter;
+    private String filetype;
+    private String key;
+    private String outputtype;
+    private String password;
+    private String region;
+    private Thumbnail thumbnail;
+    private String title;
+    private String token;
+    private String url;
+}
---
+++ src/main/java/com/onlyoffice/model/convert/thumbnail/Thumbnail.java
@@ -0,0 +1,19 @@
+package com.onlyoffice.model.convert.thumbnail;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Thumbnail {
+    private int aspect;
+    private boolean first;
+    private int height;
+    private int width;
+}
