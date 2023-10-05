660d0dc6002a562353a0ead4ccae4fc23466e97b
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-10-05 18:23:00 +0300

    added: docs for DocumentManager
    # Conflicts:
    #	src/main/java/com/onlyoffice/manager/document/DefaultDocumentManager.java
    #	src/main/java/com/onlyoffice/manager/document/DocumentManager.java

--- src/main/java/com/onlyoffice/manager/document/DefaultDocumentManager.java
+++ src/main/java/com/onlyoffice/manager/document/DefaultDocumentManager.java
@@ -42,25 +42,36 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class DefaultDocumentManager implements DocumentManager {
+
+    /**
+     * Default maximum file size, used if "settings.properties" does not specify
+     * "integration-sdk.data.filesize.editing.max" or "integration-sdk.data.filesize.conversion.max" properties.
+     */
     private static final int DEFAULT_MAX_FILE_SIZE = 5242880;
 
+    /** The {@link SettingsManager}. */
     @Getter(AccessLevel.PROTECTED)
     private SettingsManager settingsManager;
 
+    /** The list containing data about supported formats. */
     private static List<Format> formats;
 
     static {
         init();
     }
 
+    @Override
     public List<Format> getFormats() {
         return this.formats;
     }
 
+    @Override
     public abstract String getDocumentKey(String fileId, boolean embedded);
 
+    @Override
     public abstract String getDocumentName(String fileId);
 
+    @Override
     public String getExtension(final String fileName) {
         if (fileName == null) {
             return null;
@@ -79,6 +90,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return extension.toLowerCase();
     }
 
+    @Override
     public String geBaseName(final String fileName) {
         if (fileName == null) {
             return null;
@@ -97,6 +109,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return baseName;
     }
 
+    @Override
     public DocumentType getDocumentType(final String fileName) {
         String fileExtension = getExtension(fileName);
 
@@ -109,6 +122,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return null;
     }
 
+    @Override
     public boolean isEditable(final String fileName) {
         String extension = getExtension(fileName);
 
@@ -125,15 +139,18 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return hasAction(fileName, "edit");
     }
 
+    @Override
     public boolean isViewable(final String fileName) {
         return hasAction(fileName, "view");
     }
 
+    @Override
     public boolean isFillable(final String fileName) {
         return hasAction(fileName, "fill");
     }
 
 
+    @Override
     public boolean hasAction(final String fileName, final String action) {
         String fileExtension = getExtension(fileName);
 
@@ -150,6 +167,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return false;
     }
 
+    @Override
     public InputStream getNewBlankFile(final String extension, final Locale locale) {
         String path = "assets/document-templates/" + locale.toLanguageTag() + "/new." + extension;
 
@@ -162,6 +180,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
                 .getResourceAsStream(path);
     }
 
+    @Override
     public String getDefaultExtension(final DocumentType documentType) {
         if (documentType == null) {
             return null;
@@ -179,6 +198,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         }
     }
 
+    @Override
     public String getDefaultConvertExtension(final String fileName) {
         String extension = getExtension(fileName);
 
@@ -216,6 +236,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return null;
     }
 
+    @Override
     public List<String> getConvertExtensionList(final String fileName) {
         String extension = getExtension(fileName);
 
@@ -287,6 +308,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return result;
     }
 
+    @Override
     public long getMaxFileSize() {
         long size;
         try {
@@ -299,6 +321,7 @@ public abstract class DefaultDocumentManager implements DocumentManager {
         return size > 0 ? size : DEFAULT_MAX_FILE_SIZE;
     }
 
+    @Override
     public long getMaxConversionFileSize() {
         long size;
         try {
--- src/main/java/com/onlyoffice/manager/document/DocumentManager.java
+++ src/main/java/com/onlyoffice/manager/document/DocumentManager.java
@@ -27,25 +27,180 @@ import java.util.Locale;
 import java.util.Map;
 
 public interface DocumentManager {
+
+    /**
+     * Returns list containing of file formats (electronic documents, forms, spreadsheets, presentations) supported
+     * by ONLYOFFICE editors and describes the properties of each file format type, was taken from <a target="_top"
+     * href="https://github.com/ONLYOFFICE/document-formats">Formats repository</a>.
+     *
+     * @return list containing data about supported formats
+     */
     List<Format> getFormats();
+
+    /**
+     * Generate the unique document identifier used by the service to recognize the document.
+     *
+     * @param fileId the ID of the file
+     * @param embedded set true if editor opened with embedded type
+     * @return the unique document identifier
+     */
     String getDocumentKey(String fileId, boolean embedded);
+
+    /**
+     * Gets the document name by file id.
+     *
+     * @param fileId the ID of the file
+     * @return the document name
+     */
     String getDocumentName(String fileId);
+
+    /**
+     * Gets the extension of a fileName.
+     *
+     * @param fileName the fileName to retrieve the extension of
+     * @return the extension of the file or an empty string if none exists or null if the fileName is null.
+     */
     String getExtension(String fileName);
+
+
+    /**
+     * Gets the base name, minus the full path and extension, from a full fileName.
+     *
+     * @param fileName the fileName to query, null returns null
+     * @return the name of the file without the path, or an empty string if none exists
+     */
     String geBaseName(String fileName);
+
+    /**
+     * Gets the content type by file name.
+     *
+     * @param fileName the fileName to query
+     * @return the content type of the file, or "application/octet-stream" if the content type cannot be determined
+     */
     String getMimeType(String fileName);
+
+    /**
+     * Gets the document type by file name.
+     *
+     * @param fileName the fileName to retrieve the document type of
+     * @see DocumentType
+     * @return the document type
+     */
     DocumentType getDocumentType(String fileName);
+
+    /**
+     * Determine whether a document is editable by file name.
+     *
+     * @param fileName the file name to determine
+     * @return true if document is editable
+     */
     boolean isEditable(String fileName);
+
+    /**
+     * Determine whether a document is viewable by file name.
+     *
+     * @param fileName the file name to determine
+     * @return true if document is viewable
+     */
     boolean isViewable(String fileName);
+
+    /**
+     * Determine whether a document is fillable by file name.
+     *
+     * @param fileName the file name to determine
+     * @return true if document is fillable
+     */
     boolean isFillable(String fileName);
+
+    /**
+     * Determine whether an action can be performed on a document by file name.
+     *
+     * @param fileName the file name to determine
+     * @param action the performed action
+     * @see <a target="_top" href="https://github.com/ONLYOFFICE/document-formats">Formats repository</a>
+     * @return true if action can be performed on a document
+     */
     boolean hasAction(String fileName, String action);
+
+    /**
+     * Gets an {@link InputStream} of an empty file with the given extension and locale.
+     *
+     * @param extension the file extension
+     * @param locale the locale
+     * @see <a target="_top"
+     * href="https://github.com/ONLYOFFICE/document-templates/tree/main/new">Template repository</a>
+     * @return {@link InputStream} of an empty file
+     */
     InputStream getNewBlankFile(String extension, Locale locale);
+
+    /**
+     * Determines the default OOXML extension by document type.
+     *
+     * @param documentType document type
+     * @see DocumentType
+     * @return default OOXML extension
+     */
     String getDefaultExtension(DocumentType documentType);
+
+    /**
+     * Determine the default OOXML extension to which the document should be converted.
+     *
+     * @param fileName the file name to determine
+     * @return default convert OOXML extension
+     */
     String getDefaultConvertExtension(String fileName);
+
+    /**
+     * Determine a list of extensions that the document can be converted to.
+     *
+     * @param fileName the file name to determine
+     * @return list extension
+     */
     List<String> getConvertExtensionList(String fileName);
-    Map<String, Boolean> getLossyEditableMap();
+
+    /**
+     * Get a map of extensions that can be edited with possible loss of information.
+     *
+     * @return map extension
+     */
+    List<String> Map<String, Boolean> getLossyEditableMap();();
+
+    /**
+     * Get a list of extensions that can be used for insert as image to document.
+     *
+     * @return list extension
+     */
     List<String> getInsertImageExtensions();
+
+    /**
+     * Get a list of extensions that can be used for insert as image to document.
+     *
+     * @see <a target="_top" href="https://api.onlyoffice.com/editors/methods#insertImage">API ONLYOFFICE</a>
+     * @return list extension
+     */
     List<String> getCompareFileExtensions();
+
+    /**
+     * Get a list of extensions that can be used for document comparison functions.
+     *
+     * @see <a target="_top"
+     * href="https://api.onlyoffice.com/editors/config/events#onRequestCompareFile">API ONLYOFFICE</a>
+     * @return list extension
+     */
     List<String> getMailMergeExtensions();
+
+
+    /**
+     * Get the maximum file size that can be opened in the ONLYOFFICE editor.
+     *
+     * @return file size
+     */
     long getMaxFileSize();
+
+    /**
+     * Get the maximum file size that can be converted in the ONLYOFFICE Conversion Service.
+     *
+     * @return file size
+     */
     long getMaxConversionFileSize();
 }
