249d32e52c592cb271c72f3bdf9580f05523f4ca
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-11-07 14:14:16 +0300

    added: docs for model/common

--- src/main/java/com/onlyoffice/model/common/Changes.java
+++ src/main/java/com/onlyoffice/model/common/Changes.java
@@ -6,12 +6,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Defines the changes from the history object returned after saving the document.
+ */
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Changes {
+    /**
+     * Defines the document version creation date.
+     */
     private String created;
+
+    /**
+     * Defines the user who is the author of the document version.
+     */
     private User user;
 }
--- src/main/java/com/onlyoffice/model/common/User.java
+++ src/main/java/com/onlyoffice/model/common/User.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Defines the user currently viewing or editing the document.
+ */
 @Getter
 @Setter
 @Builder
@@ -14,7 +17,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
 public class User {
+    /**
+     * Defines the identification of the user. The length is limited to 128 symbols.
+     * This information is stored and used to distinguish co-authors,
+     * indicate the author of the last changes when saving and highlighting history (in the list of changes),
+     * and count users with access for a license based on the number of users.
+     * We recommend using some unique anonymized hash.
+     * Do not use sensitive data, like name or email for this field.
+     */
     private String id;
+
+    /**
+     * Defines the full name of the user. The length is limited to 128 symbols.
+     * Used since version 4.2
+     */
     private String name;
+
+    /**
+     * Defines the group the user belongs to.
+     */
     private String group;
 }
