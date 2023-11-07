b7cdc6bd082ddcf2d295a98ae7e5747efc9d240e
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-11-07 14:05:55 +0300

    fix: DocumentManger docs

--- src/main/java/com/onlyoffice/manager/request/RequestManager.java
+++ src/main/java/com/onlyoffice/manager/request/RequestManager.java
@@ -19,6 +19,7 @@
 package com.onlyoffice.manager.request;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.onlyoffice.manager.settings.SettingsManager;
 import com.onlyoffice.model.common.RequestEntity;
 import com.onlyoffice.model.common.RequestedService;
 import com.onlyoffice.model.security.Credentials;
@@ -27,15 +28,59 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 
 public interface RequestManager {
+    /**
+     * Executes a POST request to the specified service. The authorization data and the address of the document server
+     * are taken from {@link SettingsManager}.
+     *
+     * @param <R> The result type.
+     * @param requestedService The requested service.
+     * @param requestEntity The request entity.
+     * @param callback The callback method.
+     * @see SettingsManager
+     * @see RequestedService
+     * @see RequestEntity
+     * @return The result of the execution callback method.
+     */
     <R> R executePostRequest(RequestedService requestedService, RequestEntity requestEntity, Callback<R> callback)
             throws Exception;
 
+    /**
+     * Executes a POST request to the specified service. The authorization data and the address of the document server
+     * are taken from {@link SettingsManager}.
+     *
+     * @param <R> The result type.
+     * @param url The URL address to the document server.
+     * @param requestEntity The request entity.
+     * @param credentials The credentials.
+     * @param callback The callback method.
+     * @see RequestEntity
+     * @see Credentials
+     * @return The result of the execution callback method.
+     */
     <R> R executePostRequest(String url, RequestEntity requestEntity, Credentials credentials, Callback<R> callback)
             throws Exception;
 
+
+    /**
+     * Execute a POST request to the specified service with the {@link HttpUriRequest} parameter.
+     *
+     * @param <R> The result type.
+     * @param request The {@link HttpUriRequest} request.
+     * @param callback The callback method.
+     * @return The result of the execution callback method.
+     */
     <R> R executeRequest(HttpUriRequest request, Callback<R> callback)
             throws Exception;
 
+    /**
+     * Returns jwt signed http request.
+     *
+     * @param url The URL address to the document server.
+     * @param requestEntity The request entity.
+     * @param credentials The credentials.
+     * @return The jwt signed http request.
+     * @throws JsonProcessingException
+     */
     HttpPost createPostRequest(String url, RequestEntity requestEntity, Credentials credentials)
             throws JsonProcessingException;
 
