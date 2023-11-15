b9989249b220b4c3a84d5a77c2142a404c2be74a
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-11-15 13:09:06 +0300

    fix: (b9b34540b76a854dc641d8d72bc903434df51ee3)

--- src/main/java/com/onlyoffice/manager/security/DefaultJwtManager.java
+++ src/main/java/com/onlyoffice/manager/security/DefaultJwtManager.java
@@ -21,13 +21,11 @@ package com.onlyoffice.manager.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onlyoffice.manager.settings.SettingsManager;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.json.JSONObject;
 
 import java.util.Base64;
 import java.util.Map;
